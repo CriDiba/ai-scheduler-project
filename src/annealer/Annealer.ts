31ca1ce651e9f453d06e8dddaa0c7d5bda32dcc2
Kaskeeeee <64480145+Kaskeeeee@users.noreply.github.com>
2022-02-27 20:26:14 +0100

    project configurations init

---
+++ src/annealer/Annealer.ts
@@ -0,0 +1,55 @@
+import { AnnealingOptions } from '.'
+
+export class Annealer<I, S, SC> {
+  private temperature: number
+  private currentValue: number
+
+  constructor(
+    private initialize: (instance: I) => Promise<S>,
+    private solutionValue: (instance: I, solution: S) => Promise<number>,
+    private chooseRandomChange: () => Promise<SC>,
+    private transition: (instance: I, prevSolution: S, changes: SC) => Promise<{ revertChange: SC; delta: number }>
+  ) {
+    this.temperature = 0.0
+    this.currentValue = 0.0
+  }
+
+  public get lastSolutionValue(): number {
+    return this.currentValue
+  }
+
+  async anneal(instance: I, options: AnnealingOptions): Promise<S> {
+    this.temperature = options.initialTemp
+    const solution = await this.initialize(instance)
+    this.currentValue = await this.solutionValue(instance, solution)
+    for (let i = 1; options.coolingSteps; i++) {
+      this.cooldown(instance, solution, options)
+    }
+
+    return solution
+  }
+
+  private async cooldown(instance: I, solution: S, options: AnnealingOptions) {
+    this.temperature = this.temperature * options.coolingFraction
+    const startValue = this.currentValue
+
+    for (let j = 1; j <= options.stepsPerTemp; j++) {
+      const change = await this.chooseRandomChange()
+      const flip = Math.random()
+      const transition = await this.transition(instance, solution, change)
+      const exponent = -transition.delta / this.currentValue / (options.K * this.temperature)
+      const merit = Math.pow(Math.E, exponent)
+      if (transition.delta < 0) {
+        this.currentValue = this.currentValue + transition.delta
+      } else if (merit > flip) {
+        this.currentValue = this.currentValue + transition.delta
+      } else {
+        await this.transition(instance, solution, transition.revertChange)
+      }
+    }
+
+    if (this.currentValue - startValue < 0.0) {
+      this.temperature = this.temperature / options.coolingFraction
+    }
+  }
+}
---
+++ src/annealer/index.ts
@@ -0,0 +1,7 @@
+export type AnnealingOptions = {
+  initialTemp: number
+  coolingSteps: number
+  coolingFraction: number
+  stepsPerTemp: number
+  K: number
+}
